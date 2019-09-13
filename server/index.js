686c0566efb20d4f8918b2ea8c6061eefe7ae227
bloom510 <bloomjazzer@gmail.com>
2019-09-12 20:01:35 -0700

    Restores server to procedural instead of func

--- server/index.js
+++ server/index.js
@@ -1,39 +1,33 @@
-async function server() {
-    /* Core modules / environment config */
-    require('dotenv').config()
-    const path = require('path');
-    const http = require('http')
+/* Core modules / environment config */
+require('dotenv').config()
+const path = require('path');
+const http = require('http')
 
-    /* Express */
-    const express = require('express');
-    const router = require('./router');
+/* Express */
+const express = require('express');
+const router = require('./router');
 
-    /* Express middleware */
-    const morgan = require('morgan');
-    const cors = require('cors');
-    const bodyParser = require('body-parser');
+/* Express middleware */
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-    /* Utils */
-    const tunnel = require('../utils/tunnel')
+/* Utils */
+const tunnel = require('../utils/tunnel')
 
-    /* Server config */
-    const app = express();
-    const server = http.createServer(app)
-    const PORT = process.env.PORT || 8080;
-    server.listen(PORT, async () => {
-        /* Server is listening! */
-        console.log(`Listening at ${PORT}`)
-        await tunnel.instance.connect();
-        router(app, tunnel)
-    })
+/* Server config */
+const app = express();
+const server = http.createServer(app)
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, async () => {
+    /* Server is listening! */
+    console.log(`Listening at ${PORT}`)
+    await tunnel.instance.connect();
+    router(app, tunnel)
+})
 
-    /* Middleware config */
-    app.use(bodyParser.json({ type: '*/*' }));
-    app.use(express.static(path.resolve(__dirname, '../build')));
-    app.use(morgan('dev'))
-    app.use(cors())
-    
-    return true;
-}
-
-module.exports = server;
+/* Middleware config */
+app.use(bodyParser.json({ type: '*/*' }));
+app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(morgan('dev'))
+app.use(cors())
